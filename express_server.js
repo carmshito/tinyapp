ffc82c53c1b02c4f07822a656bbf2c81499d4f80
Carmelia Shito <carmshito@hotmail.com>
2023-05-30 21:07:30 -0400

    add GET route to show form, add POST route to recieve form submission, add middleware to parse the body

--- express_server.js
+++ express_server.js
@@ -4,25 +4,15 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+// app.use(express.urlencoded({ extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 // display URL index
@@ -32,9 +22,22 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// display new URL submission
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 // display single URL
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
+});
+
+// post route to receive form submission
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
