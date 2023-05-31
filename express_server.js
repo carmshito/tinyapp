690200c18ba095ac333acab2f447fdbb0724bd19
Carmelia Shito <carmshito@hotmail.com>
2023-05-31 13:04:27 -0400

    update post request to redirect to /urls/:id

--- express_server.js
+++ express_server.js
@@ -43,9 +43,9 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// POST - receive form submission
+// POST - receive form submission // redirects to /urls/:id
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.redirect("/urls/:id");
 });
