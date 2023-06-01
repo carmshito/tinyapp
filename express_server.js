7f24122ea9570ad6b5061e5b9616f0a1ba3c5341
Carmelia Shito <carmshito@hotmail.com>
2023-06-01 00:47:31 -0400

    add post route for /logout that clears the cookie and redirects the user to /urls

--- express_server.js
+++ express_server.js
@@ -97,6 +97,13 @@ app.post("/urls/:id", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  res.cookie('username', username);
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+// POST - logout endpoint
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
