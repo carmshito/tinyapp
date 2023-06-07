3cce45329cbb0f6e9954a37b468fe1e88c628203
Carmelia Shito <carmshito@hotmail.com>
2023-06-06 22:07:29 -0400

    add basic permissions features to routes

--- express_server.js
+++ express_server.js
@@ -41,13 +41,11 @@ app.listen(PORT, () => {
 });
 
 // Helper function to generate random short URL
-
 const generateRandomString = () => {
   return Math.random().toString(36).substring(2, 8);
 };
 
 // Helper function to look up emails
-
 const getUserByEmail = (email, usersDB) => {
   for (const key in usersDB) {
     if (email === usersDB[key].email) {
@@ -58,7 +56,6 @@ const getUserByEmail = (email, usersDB) => {
 };
 
 // Helper function to confirm if passwords match
-
 const passwordMatcher = (password, usersDB) => {
   for (const key in usersDB) {
     if (password === usersDB[key].password) {
@@ -69,7 +66,6 @@ const passwordMatcher = (password, usersDB) => {
 };
 
 // Helper function to find user
-
 const findUserID = (email, usersDB) => {
   for (const key in usersDB) {
     if (email === usersDB[key].email) {
@@ -82,7 +78,6 @@ const findUserID = (email, usersDB) => {
 /////////////////// ROUTES ///////////////////
 
 // GET - display URL index
-
 app.get("/urls", (req, res) => {
   const templateVars = {
     user: users[req.cookies["userID"]],
@@ -91,9 +86,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// GET - display new URL submission
-
+// GET - display new URL submission for logged in
 app.get("/urls/new", (req, res) => {
+  const userID = req.cookies["userID"];
+  if (!userID) {
+    res.redirect("/login");
+    return;
+  }
   const templateVars = {
     user: users[req.cookies["userID"]]
   };
@@ -101,43 +100,64 @@ app.get("/urls/new", (req, res) => {
 });
 
 // GET - display single URL
-
 app.get("/urls/:id", (req, res) => {
+  const existingURL = urlDatabase[req.params.id];
+  if (!existingURL) {
+    res.send("This short URL does not exist!");
+    return;
+  }
   const templateVars = {
     user: users[req.cookies["userID"]],
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
   };
   res.render("urls_show", templateVars);
-  // console.log(urlDatabase);
+  console.log(urlDatabase);
 });
 
 // GET - handle shortURL requests to redirect to its longURL
-
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
+  if (!longURL) {
+    res.send("This short URL does not exist!");
+    return;
+  }
   res.redirect(longURL);
 });
 
 // GET - register new users
-
 app.get("/register", (req, res) => {
-  const templateVars = {
-    user: users[req.cookies["userID"]]
-  };
-  res.render("urls_registration", templateVars);
+  const userID = req.cookies["userID"];
+  if (!userID) {
+    const templateVars = {
+      user: users[req.cookies["userID"]]
+    };
+    res.render("urls_registration", templateVars);
+    return;
+  }
+  res.redirect("/urls");
 });
 
+// GET - login
 app.get("/login", (req, res) => {
-  const templateVars = {
-    user: users[req.cookies["userID"]]
-  };
-  res.render("urls_login", templateVars);
+  const userID = req.cookies["userID"];
+  if (!userID) {
+    const templateVars = {
+      user: users[req.cookies["userID"]]
+    };
+    res.render("urls_login", templateVars);
+    return;
+  }
+  res.redirect("/urls");
 });
 
 // POST - receive form submission // redirects to /urls/:id
-
 app.post("/urls", (req, res) => {
+  const userID = req.cookies["userID"];
+  if (!userID) {
+    res.status(401).send("You must be logged in to use TinyApp!");
+    return;
+  }
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL;
@@ -145,7 +165,6 @@ app.post("/urls", (req, res) => {
 });
 
 // POST - deletes a URL
-
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
@@ -153,7 +172,6 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 // POST - updates a URL resource
-
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const updatedURL = req.body.updatedURL;
@@ -162,25 +180,21 @@ app.post("/urls/:id", (req, res) => {
 });
 
 // POST - login endpoint
-
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
   // [x] if the email and password are empty strings, send a 400 status code
-  
   if (!email || !password) {
     return res.status(400).send("You must provide an email and password");
   }
 
   // [x] if a user with that email cannot be found, return a 403 status code
-  
   if (!getUserByEmail(email, users)) {
     return res.status(403).send("This email is not registered");
   }
 
   // [x] if a user with that email is located, compare the password given in the form with the existing user's pswd, if it doesn't match, return 403 status code
-  
   if (!passwordMatcher(password, users)) {
     return res.status(403).send("The email or password does not match");
   }
@@ -193,7 +207,6 @@ app.post("/login", (req, res) => {
 });
 
 // POST - register endpoint
-
 app.post("/register", (req, res) => {
 
   const userID = generateRandomString();
@@ -210,9 +223,7 @@ app.post("/register", (req, res) => {
   if (getUserByEmail(email, users)) {
     return res.status(400).send("There is an account already registered with this email");
   }
-
-  //////////////////////////////////////////
-
+  
   users[userID] = {
     id: userID,
     email: email,
